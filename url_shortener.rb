08c060c621cd78e88fbe871f0d878a5d813111d0
magicjascha <magicjascha@riseup.net>
2021-01-31 18:45:44 +0100

    post to session

--- url_shortener.rb
+++ url_shortener.rb
@@ -1,12 +1,21 @@
 require "sinatra"
 
 class UrlShortener < Sinatra::Application
+  enable :sessions
 
   get '/' do
     redirect '/shortcuts/new'
   end
 
   get "/shortcuts/new" do
+    @shortcuts = session[:shortcuts]
     erb :new
+  end
+
+  post "/shortcuts" do
+    long_url = params["long_url"]
+    session[:shortcuts] = {} unless session[:shortcuts]
+    session[:shortcuts][long_url[-4..-1]] = long_url
+    redirect '/shortcuts/new'
   end
 end
