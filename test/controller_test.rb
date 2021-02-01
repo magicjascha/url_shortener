76e2e3cc167aebe305948a947200afd7e0ab1f01
magicjascha <magicjascha@riseup.net>
2021-02-01 12:50:39 +0100

    add controller test

---
+++ test/controller_test.rb
@@ -0,0 +1,55 @@
+ENV['APP_ENV'] = 'test'
+
+require 'test/unit'
+require 'rack/test'
+require 'minitest/autorun'
+require 'byebug'
+require './url_shortener'
+
+class ControllerTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    UrlShortener
+  end
+
+  def setup
+    Mongoid.purge!
+    @long_url = 'https://github.com/teamcapybara/capybara'
+  end
+
+  def test_get_root_redirects_to_new
+    response = get("/")
+    assert_equal(302, response.status)
+    assert(response.header["location"].end_with?("/shortcuts/new"))
+  end
+
+  def test_get_new_works
+    response = get("/shortcuts/new")
+    assert_equal(200, response.status)
+  end
+
+  def test_post_valid_url_saves_to_database
+    nr_database_records = Shortcut.all.count
+    #post short_url
+    post('/shortcuts', { "long_url": @long_url})
+    #check that there is one new database record with the input-long_url
+    assert_equal(nr_database_records+1, Shortcut.all.count)
+    assert(Shortcut.find_by(long_url: @long_url).present?)
+  end
+
+  def test_post_valid_url_redirects_to_new
+    response = post('/shortcuts', { "long_url": @long_url})
+    assert_equal(302, response.status)
+    assert(response.header["location"].end_with?("/shortcuts/new"))
+  end
+
+  def test_get_short_url_redirects
+    #setup a record
+    shortcut = Shortcut.create_unique_token( @long_url)
+    #test
+    response = get("/#{shortcut.token}")
+    assert_equal(302, response.status)
+    assert_equal(shortcut.long_url, response.header["location"])
+  end
+end
